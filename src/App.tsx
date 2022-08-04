792fa9986fb9360d091bf941b77b3e127fad517a
eliseekn <eliseekn@gmail.com>
2022-08-04 21:02:49 +0000

    Installation and setup of tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import React from "react"
 
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">
+    return <h1 className="text-5xl font-bold underline text-red-900">
         Resume Builder
     </h1>
 }
